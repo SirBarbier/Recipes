3367476e4284d75613afe5e7b98cc310857c2417
SirBarbier <matthew-7.7@hotmail.com>
2016-03-21 11:02:41 +0000

    Added listview

--- app/src/main/java/com/matthewbarbier/admin/recipes/MainActivity.java
+++ app/src/main/java/com/matthewbarbier/admin/recipes/MainActivity.java
@@ -2,6 +2,8 @@ package com.matthewbarbier.admin.recipes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.recipe_list_item, R.id.listViewTemplateField, createData());
+
+        ListView lv = (ListView)findViewById(R.id.listView);
+
+        lv.setAdapter(adapter);
+    }
+
+    private String[] createData()
+    {
+        String [] recipeList  = {
+            "Spag bol", "Tuna pasta bake", "Carbonara"
+        };
+
+        return recipeList;
     }
 }
